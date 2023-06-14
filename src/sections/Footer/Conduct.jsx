ab17390249d6e7af47999e07f013112f9e90945a
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2023-06-14 15:22:52 -0400

    work in progress

---
+++ src/sections/Footer/Conduct.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Conduct.scss";
+
+const Conduct = () => (
+    <React.Fragment id="conduct">
+        <div className="conduct-container">
+          <div className="conduct-shawdow-container">
+            <a href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf" >❤️ Code of Conduct</a>
+          </div>
+        </div>
+    </React.Fragment>
+);
+
+export default Conduct;
