6f8dbf66c9d54f19cf7c27dc00e6a637edec6fac
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2023-06-30 14:05:26 -0400

    faq text change

--- src/sections/FAQ/FAQ.jsx
+++ src/sections/FAQ/FAQ.jsx
@@ -11,30 +11,44 @@ const FAQ = () => (
       <h1 className="faq-title">Frequently Asked Questions</h1>
       <div className="faq-questions">
         <div className="faq-left-col">
-          <ExpandableCard color = "pink" question={"What is a hackathon?"}>
+          <ExpandableCard question={"What is a hackathon?"}>
             A hackathon is an event where students work together to build a technology-based solution to a problem. You can create an app, a website, a browser extension, whatever you’d like. The sky’s the limit, and we’ll be here to help along the way!
           </ExpandableCard>
-          <ExpandableCard color = "turqoise" question={"Is this event only for Waterloo students?"}>
-            No! Our event is open to all women and nonbinary people in high school or post-secondary.
+          <ExpandableCard question={"Who can participate?"}>
+            <span style={{ fontWeight: "600", fontSize: "18px" }}>Hacker</span> <br /><br />
+            Our event is open to students from ALL backgrounds with ANY level of
+            experience who identify as a women+ individual: woman, non-binary,
+            or other under-represented genders.  Student attendees can be enrolled
+            at any level of study at a traditional (high school, university),
+            or non-traditional institution (code bootcamp, home school, online).
+            <br /><br />
+            Join our <a style={{ fontWeight: "400" }} href="https://docs.google.com/forms/d/e/1FAIpQLSdUCqXqrW53nkw3xM5MDDjreqfnCNYGuIMcZZe_EvKZItrgiw/viewform">hacker mailing list</a> to keep up to date on when applications open!
+            <br /><br />
+            <span style={{ fontWeight: "600", fontSize: "18px" }}>Non-Hacker (eg. Mentor/Sponsor/Judge...etc)</span> <br /><br />
+            Anyone not included above is invited to apply as a mentor or support us as a sponsor! <br /><br />
+            Join our <a style={{ fontWeight: "400" }} href="https://docs.google.com/forms/d/e/1FAIpQLSdUCqXqrW53nkw3xM5MDDjreqfnCNYGuIMcZZe_EvKZItrgiw/viewform">mentor mailing list</a> to keep up to date on when application opens:
           </ExpandableCard>
-          <ExpandableCard question={"How many people can form a team?"}>
-          You can apply with or without a team and we will set you up with one!  There will be plenty of opportunities to connect with your teammates and others during the hackathon.
+          <ExpandableCard question={"How do teams work?"}>
+            Teams can consist of up to 4 hackers. You can apply with or without a team and we will set you up with one!  There will be plenty of opportunities to connect with your teammates and others during the hackathon.
           </ExpandableCard>
-          <ExpandableCard color = "pink" question={"What kind of workshops are there?"}>
-            There will be a variety of workshops, including both technical and career-related topics. Additionally, there will be workshops for both beginner and advanced hackers. Some examples could be Resume Writing, Networking, Intro to X, Artifical Intelligence. 
+          <ExpandableCard question={"What kind of workshops are there?"}>
+            There will be a variety of workshops, including both technical and career-related topics. Additionally, there will be workshops for both beginner and advanced hackers. Some examples could be Resume Writing, Networking, Intro to X, Artificial Intelligence.
           </ExpandableCard>
         </div>
         <div className="faq-right-col">
-          <ExpandableCard color = "pink" question={"What if I don't know how to code?"}>
+          <ExpandableCard question={"What if it's my first hackathon?"}>
+            No worries! Our event, Prepare with Us, will get you up to speed, so you're ready to hack during the weekend! We will also have mentors available to help guide you through your projects during the Hack with Us event.
+          </ExpandableCard>
+          <ExpandableCard question={"What if I don’t like coding?"}>
           That's ok! Hackers have many roles including project management and prototyping! Your team will need people to help ideate, design, and pitch your hack in order to bring it to life, so your unique skills will definitely be an asset!
           </ExpandableCard>
-          <ExpandableCard color = "turqoise" question={"Why is this event women+ only?"}>
+          <ExpandableCard question={"Why is this event women+ only?"}>
             Women+ are underrepresented in this field, and although there’s been progress, there is still a long way to go! Thus we want to create a safe and welcoming space for our hackers to grow and create unique projects.
           </ExpandableCard>
           <ExpandableCard question={"Is there a cost to attend?"}>
             TechNova is free to attend!
           </ExpandableCard>
-          <ExpandableCard color = "pink" question={"I applied, what happens next?"}>
+          <ExpandableCard question={"I applied, what happens next?"}>
             We will be reviewing applications on a rolling basis, so keep an eye on your email - that's how you’ll hear back from us about decisions.
           </ExpandableCard>
         </div>
