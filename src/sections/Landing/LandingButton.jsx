c9fa3a4242f6d617aa1aaeb151a8f56b47faba94
Rachelle Fontanilla <rachfontanilla@gmail.com>
2023-07-01 16:20:36 -0400

    21: open links in new tab

--- src/sections/Landing/LandingButton.jsx
+++ src/sections/Landing/LandingButton.jsx
@@ -4,7 +4,7 @@ import pixelHeart from "../../resources/images/icons/pixel-heart.svg";
 import arrow from "../../resources/images/icons/arrow.svg";
 
 const LandingButton = ({ label, link, type }) => (
-  <a href={link} className={type ?? "landing-button"} target="_blank">
+  <a href={link} className={type ?? "landing-button"} target="_blank" rel="noreferrer noopener">
     <img className="pixel" src={pixelHeart} alt="Pixel Heart" />
     <div className="label">{label}</div>
     <img className="arrow" src={arrow} alt="Arrow" />
