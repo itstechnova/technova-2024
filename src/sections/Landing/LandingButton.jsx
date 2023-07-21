99a15746116dc7322c727e92fc2f88b3ca17d225
Rachelle Fontanilla <rachfontanilla@gmail.com>
2023-07-21 12:44:45 -0400

    add clicked state to fix button press

--- src/sections/Landing/LandingButton.jsx
+++ src/sections/Landing/LandingButton.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LandingButton.scss";
 import pixelHeart from "../../resources/images/icons/pixel-heart.svg";
 import arrow from "../../resources/images/icons/arrow.svg";
 
-const LandingButton = ({ label, link, type }) => (
-  <a href={link} className={type ?? "landing-button"} target="_blank" rel="noreferrer noopener">
-    <img className="pixel" src={pixelHeart} alt="Pixel Heart" />
-    <div className="label">{label}</div>
-    <img className="arrow" src={arrow} alt="Arrow" />
-  </a>
-);
+const LandingButton = ({ label, link, type }) => {
+
+  const [clicked, setClicked] = useState(false);
+  
+  const handleClick = () => {
+    setClicked(true);
+  };
+  
+  return (
+    <a
+    href={link}
+    className={`landing-button ${type ?? ""} ${clicked ? "clicked" : ""}`}
+    target="_blank"
+    rel="noreferrer noopener"
+    onClick={handleClick}
+    >
+      <img className="pixel" src={pixelHeart} alt="Pixel Heart" />
+      <div className="label">{label}</div>
+      <img className="arrow" src={arrow} alt="Arrow" />
+    </a>
+  );
+};
 
 export default LandingButton;
