ab5f6983d5691e6451762f593fb21c092bf10f71
Lucy Wang <wang.lucy@hotmail.com>
2023-08-17 01:53:48 -0400

    update section from designs

--- src/sections/Keynote/Keynote.jsx
+++ src/sections/Keynote/Keynote.jsx
@@ -7,30 +7,40 @@ import pixelStar from "../../resources/images/graphics/pixel-star.svg";
 function Keynote() {
     return (
         <SectionWrapper id="keynote">
-            <h1 className="keynote-title">Keynote</h1>
+            <h1 className="keynote-title">Keynote Speaker</h1>
             <hr></hr>
 
-            <div className="keynote-card">
-                <img className="keynote-card-img" src={msGoose} />
-                <div className="keynote-card-title">
-                    <div className="keynote-star">
-                        <img src={pixelStar} />
-                    </div>
-                    <h3 className="keynote-card-name">
-                        Stephanie Mills
-                    </h3>
-                    <p className="keynote-card-role">
-                        CEO and Founder of CodeGem
-                    </p>
-                </div>
-                <div className="keynote-card-desc-wrapper">
-                    <p className="keynote-card-desc">
-                    Stephanie Mills is a 2020 Thiel fellow, and the CEO and founder of CodeGem; a people-first management platform built for software teams. 
 
-                    <br></br>
-                    <br></br>
-                    CodeGem was inspired by her combined experience studying management engineering at UWaterloo and working as a software engineer at top companies like Apple, Uber, Dapper, and Deloitte.
-                    </p>
+            <div className="keynote-box">
+                <div className="keynote-header">
+                    <div className="white-box"></div>
+                    <div className="white-box"></div>
+                    <div className="white-box"></div>
+                </div>
+                <div className="keynote-card">
+                    <img className="keynote-card-img" src={msGoose} />
+                    <div className="keynote-card-title">
+                        <h3 className="keynote-card-name">
+                            Stephanie Mills
+                        </h3>
+                        <div className="keynote-card-title-box">
+                            <div className="keynote-card-star">
+                                <img src={pixelStar} />
+                            </div>
+                            <p className="keynote-card-role">
+                                CEO and Founder of CodeGem
+                            </p>
+                        </div>
+                    </div>
+                    <div className="keynote-card-desc-wrapper">
+                        <p className="keynote-card-desc">
+                        Stephanie Mills is a 2020 Thiel fellow, and the CEO and founder of CodeGem; a people-first management platform built for software teams. 
+
+                        <br></br>
+                        <br></br>
+                        CodeGem was inspired by her combined experience studying management engineering at UWaterloo and working as a software engineer at top companies like Apple, Uber, Dapper, and Deloitte.
+                        </p>
+                    </div>
                 </div>
             </div>
 
