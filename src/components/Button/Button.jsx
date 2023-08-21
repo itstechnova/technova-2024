a7081a988e5e9082a30f4b2a2e2503f8c287bc3d
Rachelle Fontanilla <rachfontanilla@gmail.com>
2023-08-21 15:54:14 -0400

    simplify logic, only apply style on hover

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,23 +1,15 @@
-import React, { useState } from "react";
+import React, {onClick, useState} from "react";
 import "./Button.scss";
 import pixelHeart from "../../resources/images/icons/pixel-heart.svg";
 import arrow from "../../resources/images/icons/arrow.svg";
 
 const Button = ({ label, link, type }) => {
-
-  const [clicked, setClicked] = useState(false);
-  
-  const handleClick = () => {
-    setClicked(true);
-  };
-  
   return (
     <a
-    href={link}
-    className={`button ${type ?? ""} ${clicked ? "clicked" : ""}`}
-    target="_blank"
-    rel="noreferrer noopener"
-    onClick={handleClick}
+      href={link}
+      className={`button ${type ?? ""}`}
+      target="_blank"
+      rel="noreferrer noopener"
     >
       <img className="pixel" src={pixelHeart} alt="Pixel Heart" />
       <div className="label">{label}</div>
