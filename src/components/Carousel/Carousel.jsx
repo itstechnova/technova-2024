1d487cb64313a193fef51b8405571663124752b0
mahzabinrashid <mahzabinrashidfariha@gmail.com>
2023-08-25 10:47:27 -0400

    added some sponsors

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -5,59 +5,66 @@ import pixelStar from "../../resources/images/graphics/pixel-star.svg";
 import pixelHeart from "../../resources/images/icons/pixel-heart.svg";
 import arrow from "../../resources/images/icons/arrow.svg";
 
-
 var colourCodes = {
-  "pink": "#f8b3b8",
-  "turquoise": "#A8EFE4",
-  "purple": "#7676E8",
-  "darkPink": "#FC6D83",
-}
+  pink: "#f8b3b8",
+  turquoise: "#A8EFE4",
+  purple: "#7676E8",
+  darkPink: "#FC6D83",
+};
 
 export const Carousel = ({ content, slideNext }) => {
-
   const next = () => {
     slideNext();
-  }
+  };
 
-    return (
-        <div className="carousel-wrapper" id="carousel">
-        <div className="carousel-box">
-          <div className="carousel-header" style={{backgroundColor: colourCodes[content.colour]}}>
-            <div className="white-box"></div>
-            <div className="white-box"></div>
-            <div className="white-box"></div>
+  return (
+    <div className="carousel-wrapper" id="carousel">
+      <div className="carousel-box">
+        <div
+          className="carousel-header"
+          style={{ backgroundColor: colourCodes[content.colour] }}
+        >
+          <div className="white-box"></div>
+          <div className="white-box"></div>
+          <div className="white-box"></div>
+        </div>
+        <div className="carousel-card">
+          <div className="carousel-img-wrapper">
+            <img className="carousel-image" src={content.image} />
           </div>
-          <div className="carousel-card">
-            <div className="carousel-img-wrapper">
-              <img className="carousel-image" src={content.image} />
-            </div>
-            <div className="carousel-text">
-              <p>
-                {content.quote}
-              </p>
+          <div className="carousel-text">
+            <p>{content.quote}</p>
+          </div>
+
+          <div className="carousel-button-section">
+            <div className="carousel-button">
+              <a
+                className="landing-button"
+                href={content.link}
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <img
+                  className="career-pixel"
+                  src={pixelHeart}
+                  alt="Pixel Heart"
+                />
+                <div className="career-text">Explore Career Page</div>
+                <img className="career-pixel" src={arrow} alt="Pixel Heart" />
+              </a>
             </div>
 
-            <div className="carousel-button-section">
-              <div className="carousel-button">
-                <a className="landing-button" href={content.link}>
-                    <img className="career-pixel" src={pixelHeart} alt="Pixel Heart" />
-                    <div className="career-text">Explore Career Page</div>
-                    <img className="career-pixel" src={arrow} alt="Pixel Heart" />
-                </a>
-              </div>
-
-              <div onClick={next} className="carousel-button">
-                  <a className="landing-button">
-                    <img className="pixel" src={pixelStar} alt="Pixel Star" />
-                    <div className="label">Next</div>
-                  </a>
-              </div>
-              
+            <div onClick={next} className="carousel-button">
+              <a className="landing-button">
+                <img className="pixel" src={pixelStar} alt="Pixel Star" />
+                <div className="label">Next</div>
+              </a>
             </div>
           </div>
+        </div>
       </div>
-      </div>
-    );
+    </div>
+  );
 };
 
 export default Carousel;
--- src/sections/Sponsor/SponsorUs.jsx
+++ src/sections/Sponsor/SponsorUs.jsx
@@ -11,6 +11,7 @@ import amazon from "../../resources/images/sponsors/amazon.png";
 import wealthsimple from "../../resources/images/sponsors/wealthsimple.png";
 import google from "../../resources/images/sponsors/googlecloud.png";
 import opentext from "../../resources/images/sponsors/opentext.jpg";
+import cloudflare from "../../resources/images/sponsors/cloudflare.png";
 
 import OurSponsors from "./OurSponsors";
 import "./SponsorUs.scss";
@@ -18,38 +19,55 @@ import LandingButton from "../Landing/LandingButton";
 
 var showcase = [
   {
+    // no quote, no link
     quote:
       "For people who like to invent, there's no better place to explore opportunities than at Amazon. We're currently hiring for Software Developers (Interns and Full Time - 2022) in Canada. Join our team and help us build the future!",
     link: "https://www.amazon.jobs/student-programs",
     image: wealthsimple,
-    colour: "pink"
+    colour: "pink",
   },
   {
+    // no quote, no link
+    quote:
+      "For people who like to invent, there's no better place to explore opportunities than at Amazon. We're currently hiring for Software Developers (Interns and Full Time - 2022) in Canada. Join our team and help us build the future!",
+    link: "https://www.amazon.jobs/student-programs",
+    image: google,
+    colour: "darkPink",
+  },
+  {
+    // no image
+    quote:
+      "Zynga is a global leader in interactive entertainment with a mission to connect the world through games, come work with us!",
+    link: "https://www.zynga.com/jobs/job-openings/",
+    image: google,
+    colour: "darkPink",
+  },
+  {
+    // not a sponsor?
     quote:
       "For people who like to invent, there's no better place to explore opportunities than at Amazon. We're currently hiring for Software Developers (Interns and Full Time - 2022) in Canada. Join our team and help us build the future!",
     link: "https://www.amazon.jobs/student-programs",
     image: amazon,
-    colour: "turquoise"
+    colour: "turquoise",
   },
   {
     quote:
       "At OpenText, we recognize the low demographic of women in STEM. As part of our ongoing efforts to encourage women to participate in STEM and feel recognized for their invaluable insights, we invite you to join our Women in Tech program.",
     link: "https://careers.opentext.com/?locale=en_US",
     image: opentext,
-    colour: "purple"
-  },{
+    colour: "purple",
+  },
+  {
     quote:
-      "For people who like to invent, there's no better place to explore opportunities than at Amazon. We're currently hiring for Software Developers (Interns and Full Time - 2022) in Canada. Join our team and help us build the future!",
-    link: "https://www.amazon.jobs/student-programs",
-    image: google,
-    colour: "darkPink"
+      "The future of apps is AI - learn how you can build better assistive and generative AI apps on the Cloudflare global network.",
+    link: "https://ai.cloudflare.com/",
+    image: cloudflare,
+    colour: "purple",
   },
 ];
 
-
-
 export const SponsorUs = () => {
-  const [slidesToShow, setSlidesToShow] = useState(1.5); 
+  const [slidesToShow, setSlidesToShow] = useState(1.5);
 
   useEffect(() => {
     const handleResize = () => {
@@ -61,9 +79,9 @@ export const SponsorUs = () => {
       }
     };
     handleResize();
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
@@ -73,15 +91,15 @@ export const SponsorUs = () => {
     speed: 520,
     slidesToShow: slidesToShow,
     slidesToScroll: 1,
-    arrows: false
-  }
+    arrows: false,
+  };
   const sliderRef = useRef(null);
 
   const next = () => {
     sliderRef.current.slickNext();
-  }
+  };
 
-  return(
+  return (
     <SectionWrapper id="sponsor">
       <div className="sponsor-us-container">
         <h1 className="sponsor-us-title">Sponsor Us</h1>
@@ -93,34 +111,43 @@ export const SponsorUs = () => {
                 Support more than 300+ young women in the next step of their
                 journey by sponsoring the next generation of leaders. <br />
                 Interested in sponsoring? Email us at{" "}
-                <a href="mailto:sponsors@itstechnova.org" target="_blank" rel="noreferrer noopener">
+                <a
+                  href="mailto:sponsors@itstechnova.org"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
                   sponsors@itstechnova.org
                 </a>
               </p>
             </div>
             <div className="sponsor-us-button-container">
-              <LandingButton link="https://www.itstechnova.org/pdfs/sponsorship_package.pdf" label="Sponsorship Package"  target="_blank" rel="noreferrer noopener"/>
-              <LandingButton link="mailto:sponsors@itstechnova.org" label="Join us as a sponsor"  target="_blank" rel="noreferrer noopener"/>
+              <LandingButton
+                link="https://www.itstechnova.org/pdfs/sponsorship_package.pdf"
+                label="Sponsorship Package"
+                target="_blank"
+                rel="noreferrer noopener"
+              />
+              <LandingButton
+                link="mailto:sponsors@itstechnova.org"
+                label="Join us as a sponsor"
+                target="_blank"
+                rel="noreferrer noopener"
+              />
             </div>
           </div>
           <div className="sponsor-us-images">
-            <img
-              className="flowers"
-              src={flowers}
-              alt="plant and bee"
-            />
+            <img className="flowers" src={flowers} alt="plant and bee" />
           </div>
         </div>
         <div className="sponsor-us-carousel-wrapper">
-        <div className="sponsor-us-carousel">
-          <Slider ref={sliderRef} {...settings} >
-            <Carousel content={showcase[0]} slideNext={next} />
-            <Carousel content={showcase[1]} slideNext={next}/>
-            <Carousel content={showcase[2]} slideNext={next}/>
-            <Carousel content={showcase[3]} slideNext={next} />
-          </Slider> 
-        </div>  
-      </div>
+          <div className="sponsor-us-carousel">
+            <Slider ref={sliderRef} {...settings}>
+              {showcase.map((content, index) => (
+                <Carousel key={index} content={content} slideNext={next} />
+              ))}
+            </Slider>
+          </div>
+        </div>
         <OurSponsors />
       </div>
     </SectionWrapper>
