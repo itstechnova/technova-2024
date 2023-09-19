72ef641fe68b2f05a14aeac5d8618f41526ff1b5
Rachelle Fontanilla <rachfontanilla@gmail.com>
2023-09-19 16:51:08 -0400

    update to 2023 team pictures + names

--- src/App.jsx
+++ src/App.jsx
@@ -51,6 +51,7 @@ const App = () => {
             <SponsorUs />
             <Keynote />
             <FAQ />
+            <Team />
             <Contact />
             <FooterBar />       
           </div>
