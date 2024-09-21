b2973f282abaea49e3a69f853545f50478dc82b1
Lucy Wang <wang.lucy@hotmail.com>
2024-09-21 03:07:40 -0400

    update speaker

--- src/sections/Keynote/Keynote.jsx
+++ src/sections/Keynote/Keynote.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Keynote.scss";
 import SectionWrapper from "../../components/SectionWrapper/SectionWrapper";
-import KeynoteSpeaker from "../../resources/images/keynote.svg"
+import KeynoteSpeaker from "../../resources/images/keynoteSpeaker.jpg"
 
 function Keynote() {
     return (
@@ -12,21 +12,19 @@ function Keynote() {
 
             <div className="keynote-box">
                 <div className="keynote-header">
-                    <h3 className="keynote-header-name">CEO and Founder of CodeGem</h3>
+                    {/* <h3 className="keynote-header-name">CEO and Founder of CodeGem</h3> */}
                 </div>
                 <div className="keynote-card">
                     <img className="keynote-card-img" src={KeynoteSpeaker} />
                     <div className="keynote-card-title">
                         <h3 className="keynote-card-name">
-                        Stephanie Mills
+                        Maura Grossman
                         </h3>
                     </div>
                     <div className="keynote-card-desc-wrapper">
                         <p className="keynote-card-desc">
-                        Stephanie Mills is a 2020 Thiel fellow, and the CEO and founder of CodeGem; a people-first management platform built for software teams.
-                        <br></br>
-                        <br></br>
-                        CodeGem was inspired by her combined experience studying management engineering at UWaterloo and working as a software engineer at top companies like Apple, Uber, Dapper, and Deloitte.</p>
+                        Maura R. Grossman is a Research Professor at the Cheriton School of Computer Science and the School of Public Health Sciences at the University of Waterloo, an Adjunct Professor at Osgoode Hall Law School of York University, and a Faculty Affiliate of the Vector Institute. Maura’s groundbreaking research on technology-assisted review (TAR), high-recall information retrieval, electronic discovery in law, and AI and Data Science relating to legal, ethical, and policy considerations, has set the standard for the responsible application of AI in critical legal contexts.
+                        </p>
                     </div>
                 </div>
             </div>
