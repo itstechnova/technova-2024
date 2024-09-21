a518f42e1a84fc1c3b4d16333bf2d541fd5d52d7
Lindsay Zhang <zhlindsay04@gmail.com>
2024-09-21 19:00:25 -0400

    remove unused imports

--- src/sections/Landing/Landing.jsx
+++ src/sections/Landing/Landing.jsx
@@ -2,19 +2,7 @@ import "./Landing.scss";
 import SectionWrapper from "../../components/SectionWrapper/SectionWrapper";
 import landing from "../../resources/strings/landing";
 import landingBanner from "../../resources/images/logo/techNovaTiles.png";
-import techTogether from "../../resources/images/logo/techtogether.png";
 import Button from "../../components/Button/Button";
-import tnLogo from "../../resources/images/logo/logo.png";
-// import asterisk1 from "../../resources/images/graphics/asterisk-1.svg";
-// import asterisk2 from "../../resources/images/graphics/asterisk-2.svg";
-import asterisk3 from "../../resources/images/graphics/asterisk-3.svg";
-// import asterisk4 from "../../resources/images/graphics/asterisk-4.svg";
-// import asterisk5 from "../../resources/images/graphics/asterisk-5.svg";
-// import softStar1 from "../../resources/images/graphics/soft-star-1.svg";
-// import softStar2 from "../../resources/images/graphics/soft-star-2.svg";
-import msGoose from "../../resources/images/graphics/ms-goose.svg";
-import msGooseFlowers from "../../resources/images/graphics/ms-goose-flowers.png";
-import mlh from "../../resources/images/logo/mlh.png";
 import snakes from "../../resources/images/graphics/snakesnlatters.png"
 
 const Landing = () => {
