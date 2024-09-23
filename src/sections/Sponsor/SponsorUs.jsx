517a8fc409c0455cf219c6f14b49755f62c0c015
Shivali Sen <s27sen@uwaterloo.ca>
2024-09-23 15:28:27 -0400

    update sponsor us, buttons, and constants

--- src/sections/Sponsor/SponsorUs.jsx
+++ src/sections/Sponsor/SponsorUs.jsx
@@ -1,27 +1,9 @@
 import React, { useRef, useState, useEffect } from "react";
-import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import SectionWrapper from "../../components/SectionWrapper/SectionWrapper";
 import Button from "../../components/Button/Button";
 import { Carousel, CarouselTypes } from "../../components/Carousel/Carousel";
-
-import flowers from "../../resources/images/graphics/sponsor-us-flowers.svg";
-import amazon from "../../resources/images/sponsors/amazon.png";
-import wealthsimple from "../../resources/images/sponsors/wealthsimple.png";
-import googlecloud from "../../resources/images/sponsors/googlecloud.png";
-import opentext from "../../resources/images/sponsors/opentext.jpg";
-import cloudflare from "../../resources/images/sponsors/cloudflare.png";
-import zynga from "../../resources/images/sponsors/zynga.png";
-import sps from "../../resources/images/sponsors/sps.png";
-import bbb from "../../resources/images/sponsors/bigbluebubble.png";
-import hrt from "../../resources/images/sponsors/hrt.png";
-import bam from "../../resources/images/sponsors/bam.png";
-import citadel from "../../resources/images/sponsors/citadel.png";
-import google from "../../resources/images/sponsors/google.png";
-import scotiabank from "../../resources/images/sponsors/scotiabank.png";
-import meta from "../../resources/images/sponsors/meta.png";
-import sunlife from "../../resources/images/sponsors/sunlife.png";
 
 import OurSponsors from "./OurSponsors";
 import "./SponsorUs.scss";
@@ -32,141 +14,8 @@ var colourCodes = {
   lightPink: "#FFA3AF", //bronze sponsor
   darkPink: "#FC6D83", //startup
 
-  pink: "#f8b3b8",
-  turquoise: "#A8EFE4",
-  purple: "#7676E8",
 };
 
-var showcase = [
-  // {
-  //   // no quote, no link
-  //   quote:
-  //     "For people who like to invent, there's no better place to explore opportunities than at Amazon. We're currently hiring for Software Developers (Interns and Full Time - 2022) in Canada. Join our team and help us build the future!",
-  //   link: "https://www.amazon.jobs/student-programs",
-  //   image: wealthsimple,
-  //   colour: "pink",
-  // },
-  // {
-  //   // no quote, no link
-  //   quote:
-  //     "For people who like to invent, there's no better place to explore opportunities than at Amazon. We're currently hiring for Software Developers (Interns and Full Time - 2022) in Canada. Join our team and help us build the future!",
-  //   link: "https://www.amazon.jobs/student-programs",
-  //   image: google,
-  //   colour: "darkPink",
-  // },
-  // {
-  //   quote:
-  //     "Zynga is a global leader in interactive entertainment with a mission to connect the world through games and a wholly-owned label of Take-Two Interactive Software, Inc.",
-  //   link: "https://www.zynga.com/",
-  //   image: zynga,
-  //   colour: "pink",
-  // },
-  // {
-  //   quote:
-  //     "At OpenText, we recognize the low demographic of women in STEM. As part of our ongoing efforts to encourage women to participate in STEM and feel recognized for their invaluable insights, we invite you to join our Women in Tech program.",
-  //   link: "https://careers.opentext.com/?locale=en_US",
-  //   image: opentext,
-  //   colour: "turquoise",
-  // },
-  // {
-  //   quote:
-  //     "Cloudflare is the security, performance, and reliability company on a mission to help build a better Internet. Today it runs one of the world’s largest networks that powers anything connected to the Internet, with its services being used by everyone, from nonprofits and SMBs to Global 2000 companies and top government organizations.",
-  //   link: "https://www.cloudflare.com/",
-  //   image: cloudflare,
-  //   colour: "purple",
-  // },
-  {
-    title: "Gold Sponsor",
-    quote:
-      "SPS Commerce is the world’s leading retail network, connecting trading partners around the globe to optimize supply chain operations for all retail partners. We support data-driven partnerships with innovative cloud technology, customer-obsessed service and accessible experts so our customers can focus on what they do best.",
-    links: [
-      {
-        url: "https://www.spscommerce.com/",
-        image: sps,
-      }
-    ], 
-    colour: "lightGold",
-    sponsorTier: "gold",
-  },
-  {
-    title: "Silver Sponsor",
-    quote:
-      "Inspiring Worlds of Creativity\
-      Big Blue Bubble is an established video game developer with over 19 years of experience creating fun, innovative, and accessible titles. They develop games on a variety of platforms that inspire creativity and encourage people of all ages to embrace their playful side.",
-    links: [
-      {
-        url: "https://www.bigbluebubble.com/",
-        image: bbb,
-      }
-    ],
-    colour: "lightPurple",
-    sponsorTier: "silver",
-  },
-  {
-    title: "Bronze Sponsors",
-    quote: "",
-    links: [
-      {
-        url: "https://www.wealthsimple.com/en-ca/careers",
-        image: wealthsimple,
-      },
-      {
-        url: "https://www.cloudflare.com/",
-        image: cloudflare,
-      },
-      {
-        url: "https://www.zynga.com/",
-        image: zynga,
-      },
-      {
-        url: "https://www.opentext.com/",
-        image: opentext,
-      },
-      {
-        url: "https://www.sunlife.ca/en/",
-        image: sunlife,
-      },
-      {
-        url: "https://cloud.google.com/",
-        image: google,
-      },
-    ],
-    colour: "lightPink",
-    sponsorTier: "bronze",
-  },
-  { 
-    title: "Other Sponsors",
-    quote: "",
-    links: [
-      {
-        url: "https://www.amazon.ca/",
-        image: amazon,
-      },
-      {
-        url: "https://www.hudsonrivertrading.com/",
-        image: hrt,
-      },
-      {
-        url: "https://www.bamfunds.com/",
-        image: bam,
-      },
-      {
-        url: "https://www.citadel.com/",
-        image: citadel,
-      },
-      {
-        url: "https://www.scotiabank.com/ca/en/personal.html",
-        image: scotiabank,
-      },
-      {
-        url: "https://about.meta.com/metaverse/",
-        image: meta,
-      },
-    ],
-    colour: "turquoise",
-    sponsorTier: "other",
-  },
-];
 
 export const SponsorUs = () => {
   const [slidesToShow, setSlidesToShow] = useState(1.5);
@@ -208,7 +57,7 @@ export const SponsorUs = () => {
   return (
     <SectionWrapper id="sponsor">
       <div className="sponsor-us-container">
-        <img id="flowers" src={flowers} alt="plant and bee"/>
+        {/* <img id="flowers" src={flowers} alt="plant and bee"/> */}
 
         <h1 className="sponsor-us-title">Sponsor Us</h1>
         <hr></hr>
@@ -240,6 +89,7 @@ export const SponsorUs = () => {
                 label="Sponsorship Package"
                 target="_blank"
                 rel="noreferrer noopener"
+                type="pink"
               />
             </div>
           </div>
